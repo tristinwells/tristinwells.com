d4484398fe1d8401415120f8747130d95bece26a
tristin <tristin@192.168.0.5>
2015-06-01 17:04:40 -0600

    syntax for data scripts

--- management /commands/datascript.py
+++ management /commands/datascript.py
@@ -5,13 +5,16 @@ class Command(BaseCommand):
     help = "Creates superuser, creates non-superuser, creates 3 blog posts from non-super user"
 
     def handle(self, *args, **options):
-        
-        def superuser(self):
-            obj, created = Person.objects.get_or_create(superuser='tristin-admin')
-                    
-        def non_superuser(self):
-            obj, created = Person.objects.get_or_create(superuser='tristin', 
-                                                        defaults={'blog 1': date:(2015, 5, 20)
-                                                                  'blog 2': date:(2015,5,30)
-                                                                  'blog 3': date:(2015,5,25)})
+        self.superuser()
+        self.non_superuser()
+
+    def superuser(self):
+        obj, created = Person.objects.get_or_create(superuser='tristin-admin')
+
+    def non_superuser(self):
+        obj, created = Person.objects.get_or_create(
+            superuser='tristin',
+            defaults={'blog 1': date(2015, 5, 20),
+                      'blog 2': date(2015, 5, 30),
+                      'blog 3': date(2015, 5, 25)})
 
