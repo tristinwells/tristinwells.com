5ab319e49d8a1b6a1c7746f0dd64bbab7285c93a
tristin <tristin@192.168.0.5>
2015-06-01 15:58:11 -0600

    Managers intial data scripts

---
+++ management /commands/datascript.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand, CommandError
+from entry.models import Entry
+
+class Command(BaseCommand):
+    help = "Creates superuser, creates non-superuser, creates 3 blog posts from non-super user"
+
+    def handle(self, *args, **options):
+        
+        def superuser(self):
+            obj, created = Person.objects.get_or_create(superuser='tristin-admin')
+                    
+        def non_superuser(self):
+            obj, created = Person.objects.get_or_create(superuser='tristin', 
+                                                        defaults={'blog 1': date:(2015, 5, 20)
+                                                                  'blog 2': date:(2015,5,30)
+                                                                  'blog 3': date:(2015,5,25)})
+
