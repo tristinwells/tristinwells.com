9e54245e04f9057f56dda1cf8fc6b11e1d5308e5
tristin <tristin@192.168.0.5>
2015-06-03 16:10:16 -0600

    Index for blog

--- blog/views.py
+++ blog/views.py
@@ -1,45 +1,41 @@
 
 from django.shortcuts import render_to_response, get_object_or_404
-
-from blog.models import Blog
-from blog.models import Category
 from django.views.generic.list import ListView
-from django.views.generic.base import TemplateView
+
+from blog.models import Category
+from blog.models import Entry
+
 
 # Class based views:
 # TemplateView
 # ListView
 # DetailView
 # FormView
-
-# class BlogView(TemplateView):
-#     template_name = 'blog.html'
-
-# class BlogIndex(ListView):
+# class EntryView(TemplateView):
+#     template_name = 'Entry.html'
+# class EntryIndex(ListView):
 #     # queryset = Entry.objects.published()
 #     template_name = "home.html"
 #     paginate_by = 2
-
-
-class BlogView(ListView):
-    template_name = 'blog/blog.html'
-    model = Blog
+class EntryView(ListView):
+    template_name = 'Entry/Entry.html'
+    model = Entry
 
 
 def index(request):
     return render_to_response('index.html', {
         'categories': Category.objects.all(),
-        'posts': Blog.objects.all()[:5]
+        'posts': Entry.objects.all()[:5]
     })
 
 def view_post(request, slug):
     return render_to_response('view_post.html', {
-        'post': get_object_or_404(Blog, slug=slug)
+        'post': get_object_or_404(Entry, slug=slug)
     })
 
 def view_category(request, slug):
     category = get_object_or_404(Category, slug=slug)
     return render_to_response('view_category.html', {
         'category': category,
-        'posts': Blog.objects.filter(category=category)[:5]
+        'posts': Entry.objects.filter(category=category)[:5]
     })
