faddaade86437e61e1de193ee811bcd5cf97a2b4
tristin <tristin@192.168.0.5>
2015-06-09 16:03:12 -0600

    Add entry

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
+from django.contrib.admin.options import ModelAdmin
 
-from blog.models import Entry
 from blog.models import Category
+from blog.models import Entry
 
 
 class EntryAdmin(admin.ModelAdmin):
@@ -13,3 +14,29 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Entry, EntryAdmin)
 admin.site.register(Category, CategoryAdmin)
+
+class ReadOnlyModelAdmin(ModelAdmin):
+    """Read only model admin class that only allows reads."""
+
+    readonly_fields = []
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def get_readonly_fields(self, request, obj=None):
+        if self.readonly_fields is None:
+            self.readonly_fields = []
+
+        fields = [field.name for field in obj._meta.fields
+                  if field.name not in self.readonly_fields]
+        return self.readonly_fields + fields
+
+    def get_actions(self, request):
+        actions = super(ReadOnlyModelAdmin, self).get_actions(request)
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+
+        return actions
--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.db.models.fields import CharField
+from django_core.forms.fields import CharFieldStripped
 
 
 class ContactForm(forms.Form):
@@ -8,3 +10,17 @@ class ContactForm(forms.Form):
     def send_email(self):
         # send email using the self.cleaned_data dictionary
         pass
+
+    class CharFieldStripped(CharField):
+        """Wrapper around CharField that strips whitespace from the CharField when
+    validating so .strip() doesn't have to be called every time you validate
+    the field's data.
+    """
+
+    def clean(self, value):
+        if value:
+            value = value.strip()
+
+        return super(CharFieldStripped, self).clean(value)
+
+
